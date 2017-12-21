34da9c1126804ed88178d08c222945c95e8dd853
huangzhixing <huangzhixing@corp.corp.netease.com>
2017-12-21 09:29:05 +0800

    1.add symlink editor 2.delete unused assets

---
+++ ProjectDev/Assets/Plugins/Editor/SymlinkEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Plugins.Editor
+{
+    public class SymlinkEditor
+    {
+        [InitializeOnLoadMethod]
+        public static void BuildSymlink()
+        {
+            SymlinkUtil.CreateSymlink(Application.dataPath + "/Project", "Art",
+                "../../../ProjectArt/Assets/Project/Art");
+        }
+    }
+}
